d1edc6b15d4be830b2d2c3a5f43c23ac173e5a64
Kevin Ochieng <kevinochieng932@gmail.com>
2020-03-16 12:12:20 -0700

    robofriends first deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import CardList from './CardList';
+import { robots } from './robots';
+import SearchBox from './SearchBox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    constructor(){
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+        
+    }
+
+    render(){
+        const filteredaRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase());
+        });
+
+        return(
+            <React.Fragment>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <div className='cards-container'>
+                    <CardList robots = {filteredaRobots}/>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
-
 export default App;
